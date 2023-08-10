cd650598d422e9ddcf835c2a8c1de5dc1990d471
Paul Jacobs <neojakey@gmail.com>
2023-08-10 08:06:41 -0500

    Highlight the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -11,9 +11,10 @@ import getCroppedImageUrl from '../services/image-url';
 
 interface Props {
     onSelectGenre: (genre: Genre) => void;
+    selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
     const { data, isLoading, error } = useGenres();
 
     if (error) return null;
@@ -31,6 +32,16 @@ const GenreList = ({ onSelectGenre }: Props) => {
                             src={getCroppedImageUrl(genre.image_background)}
                         />{' '}
                         <Button
+                            fontWeight={
+                                genre.id === selectedGenre?.id
+                                    ? 'bold'
+                                    : 'normal'
+                            }
+                            color={
+                                genre.id === selectedGenre?.id
+                                    ? 'yellow'
+                                    : 'initial'
+                            }
                             onClick={() => onSelectGenre(genre)}
                             fontSize="lg"
                             variant="link"
