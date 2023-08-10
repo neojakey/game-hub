dc5ef4f534910fb688a36dd281ddc028cf296244
Paul Jacobs <neojakey@gmail.com>
2023-08-10 07:35:26 -0500

    Filter games by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,9 +3,14 @@ import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
+import { Genre } from '../hooks/useGenres';
 
-const GameGrid = () => {
-    const { data, error, isLoading } = useGames();
+interface Props {
+    selectGenre: Genre | null;
+}
+
+const GameGrid = ({ selectGenre }: Props) => {
+    const { data, error, isLoading } = useGames(selectGenre);
     const skeletons = [1, 2, 3, 4, 5, 6];
 
     return (
